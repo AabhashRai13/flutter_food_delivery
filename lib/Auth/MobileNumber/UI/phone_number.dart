ab01f7856705d76e607b28da288e8b9233e0bfca
Sujanlamichhane31 <sujanlamichhane742@gmail.com>
2022-10-25 13:10:43 +0545

    banner fixed in mobile input page

--- lib/Auth/MobileNumber/UI/phone_number.dart
+++ lib/Auth/MobileNumber/UI/phone_number.dart
@@ -33,37 +33,65 @@ class PhoneNumberState extends State<PhoneNumber> {
             mainAxisSize: MainAxisSize.max,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
-              const SizedBox(
-                height: 100,
-              ),
               SizedBox(
-                height: MediaQuery.of(context).size.height * 0.09,
-                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height * 0.15,
+              ),
+              Container(
+                alignment: Alignment.center,
+                height: MediaQuery.of(context).size.height * 0.08,
+                width: MediaQuery.of(context).size.width * 0.7,
                 child: SvgPicture.asset("images/renterii_text.svg",
                     semanticsLabel: 'Acme Logo'),
               ),
               SizedBox(
-                height: MediaQuery.of(context).size.height * 0.012,
-                width: MediaQuery.of(context).size.width * 0.6,
+                height: MediaQuery.of(context).size.height * 0.010,
+                width: MediaQuery.of(context).size.width * 0.5,
                 child: SvgPicture.asset("images/for_lenders_text.svg",
                     semanticsLabel: 'Acme Logo'),
               ),
-              const SizedBox(
-                height: 100,
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.15,
               ),
               SizedBox(
-                height: MediaQuery.of(context).size.height * 0.25,
-                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height * 0.22,
+                width: MediaQuery.of(context).size.width * 0.9,
                 child: SvgPicture.asset("images/cycle.svg",
                     semanticsLabel: 'Acme Logo'),
               ),
               const Spacer(),
-              SizedBox(
-                height: MediaQuery.of(context).size.height * 0.19,
-                width: MediaQuery.of(context).size.width,
-                child: SvgPicture.asset("images/renterii_banner.svg",
-                    semanticsLabel: 'Acme Logo'),
-              ),
+              Container(
+                  height: MediaQuery.of(context).size.height * 0.19,
+                  width: MediaQuery.of(context).size.width,
+                  alignment: Alignment.center,
+                  child: Stack(
+                    children: [
+                      SvgPicture.asset(
+                        "images/renterii_banner.svg",
+                        semanticsLabel: 'Acme Logo',
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(12.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Flexible(
+                                flex: 2,
+                                child: Padding(
+                                  padding: const EdgeInsets.only(
+                                      right: 12.0, top: 15.0),
+                                  child: Image.asset('images/rental_icon.png'),
+                                )),
+                            Flexible(
+                                flex: 5,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(30.0),
+                                  child: Image.asset('images/make_money.png'),
+                                ))
+                          ],
+                        ),
+                      )
+                    ],
+                  )),
               const Padding(
                 padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 2.0),
                 child: MobileInput(),
